201e25c17fa00e5b8ea1989eebc63a0c7efdefbc
KirCute_ECT <951206789@qq.com>
2024-12-12 20:50:00 +0800

    fix(ftp-server): large transfer leads to client timeout (#7639)
    * fix(ftp-server): client timeout to wait a large file upload to netdisk
    
    * fix(ftp-server): driver alist v3 upload failed and temp files do not be deleted

--- server/ftp/fsup.go
+++ server/ftp/fsup.go
@@ -87,5 +87,7 @@ func (f *FileUploadProxy) Close() error {
 		WebPutAsTask: false,
 	}
 	s.SetTmpFile(f.buffer)
-	return fs.PutDirectly(f.ctx, dir, s, true)
+	s.Closers.Add(f.buffer)
+	_, err = fs.PutAsTask(f.ctx, dir, s)
+	return err
 }
