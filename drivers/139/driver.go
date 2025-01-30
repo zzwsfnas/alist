b9f397d29f0a4e75c72564da40d0f297ed8c5626
abc1763613206 <abc1763613206@163.com>
2025-01-30 11:25:41 +0800

    fix(139): restore the `Account` handling, partially reverts #7850 (#7900 close #7784)

--- drivers/139/driver.go
+++ drivers/139/driver.go
@@ -2,11 +2,13 @@ package _139
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/alist-org/alist/v3/drivers/base"
@@ -69,29 +71,28 @@ func (d *Yun139) Init(ctx context.Context) error {
 	default:
 		return errs.NotImplement
 	}
-	// if d.ref != nil {
-	// 	return nil
-	// }
-	// decode, err := base64.StdEncoding.DecodeString(d.Authorization)
-	// if err != nil {
-	// 	return err
-	// }
-	// decodeStr := string(decode)
-	// splits := strings.Split(decodeStr, ":")
-	// if len(splits) < 2 {
-	// 	return fmt.Errorf("authorization is invalid, splits < 2")
-	// }
-	// d.Account = splits[1]
-	// _, err = d.post("/orchestration/personalCloud/user/v1.0/qryUserExternInfo", base.Json{
-	// 	"qryUserExternInfoReq": base.Json{
-	// 		"commonAccountInfo": base.Json{
-	// 			"account":     d.getAccount(),
-	// 			"accountType": 1,
-	// 		},
-	// 	},
-	// }, nil)
-	// return err
-	return nil
+	if d.ref != nil {
+		return nil
+	}
+	decode, err := base64.StdEncoding.DecodeString(d.Authorization)
+	if err != nil {
+		return err
+	}
+	decodeStr := string(decode)
+	splits := strings.Split(decodeStr, ":")
+	if len(splits) < 2 {
+		return fmt.Errorf("authorization is invalid, splits < 2")
+	}
+	d.Account = splits[1]
+	_, err = d.post("/orchestration/personalCloud/user/v1.0/qryUserExternInfo", base.Json{
+		"qryUserExternInfoReq": base.Json{
+			"commonAccountInfo": base.Json{
+				"account":     d.getAccount(),
+				"accountType": 1,
+			},
+		},
+	}, nil)
+	return err
 }
 
 func (d *Yun139) InitReference(storage driver.Driver) error {
